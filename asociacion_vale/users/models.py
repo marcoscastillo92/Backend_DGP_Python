fb067e75e2a9bfbf290efede40198d0f4306dabb
migueg <miguegarciatenorio@gmail.com>
2020-11-07 15:57:04 +0100

    ForumUser and MessageForumUser models

--- asociacion_vale/users/models.py
+++ asociacion_vale/users/models.py
@@ -2,6 +2,9 @@ from django.db import models
 from datetime import datetime
 import json
 import secrets
+from django.contrib.auth.models import User as Tutor
+from ckeditor.fields import RichTextField
+
 
 def user_directory_path(instance, filename): 
     # file will be uploaded to MEDIA_ROOT / user_<id>/<filename> 
@@ -37,3 +40,30 @@ class Pictograms(models.Model):
     name= models.CharField(max_length=150)
     key= models.CharField(max_length=300)
     section= models.CharField(max_length=150)
+
+
+class ForumUser (models.Model):
+    user =  models.ForeignKey(User, on_delete=models.CASCADE, null=True, verbose_name="Propietario")
+    createdAt = models.DateTimeField(default=datetime.now, blank=True, verbose_name="Fecha de creación")
+    tutor = models.ForeignKey(Tutor, on_delete=models.CASCADE, null=True, verbose_name="Tutor")
+    class Meta:
+        verbose_name = "Chat de Usuario"
+        verbose_name_plural = "Chats de Usuario"
+
+    def __str__(self):
+        return f"{self.tutor}"
+class MessageForumUser(models.Model):
+    body = RichTextField(verbose_name="Mensaje")
+    author = models.ForeignKey(User, on_delete=models.CASCADE, verbose_name="Autor")
+    mimeType = models.FileField(upload_to ='uploads/attach', default="null", verbose_name="Tipo de mensaje") 
+    forum = models.ForeignKey(ForumUser, on_delete=models.CASCADE, null=True, verbose_name="Conversacion")
+    createdAt = models.DateTimeField(default=datetime.now, blank=True, verbose_name="Fecha de creacion")
+
+    def save(self, *args, **kwargs):
+        if self.forum is None:  # Set default reference
+            self.forum = ForumUser.objects.get(id=1)
+        super(MessageForumUser, self).save(*args, **kwargs)
+
+    class Meta:
+        verbose_name = "Mensaje"
+        verbose_name_plural = "Mensajes"
