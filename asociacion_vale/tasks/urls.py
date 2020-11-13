bcfda1bfac4f8efc114cb2246720b4fb82bac05b
Marcos <marcoscastillo@correo.ugr.es>
2020-11-13 23:39:54 +0100

    refactor

--- asociacion_vale/tasks/urls.py
+++ asociacion_vale/tasks/urls.py
@@ -6,5 +6,6 @@ urlpatterns = [
     path('create/', views.createTask, name='createTask'),
     path('get/' , views.getAllTasks, name='getTasks'),
     path('get/<int:id>' , views.getTask, name='getTask'),
-    path('rate/' , views.rateTask, name='rateTask')
+    path('rate/' , views.rateTask, name='rateTask'),
+    path('delete/<int:id>', views.deleteTask, name='deleteTask')
 ]
--- asociacion_vale/tasks/views.py
+++ asociacion_vale/tasks/views.py
@@ -1,8 +1,8 @@
 from django.shortcuts import HttpResponse
-from django.http import JsonResponse
+from django.http import JsonResponse, HttpResponseBadRequest, Http404, HttpResponseNotAllowed
 from django.views.decorators.csrf import csrf_exempt
 from .models import Category, Rating, Task
-from .controller import Controller as TaskController
+import tasks.controller as TaskController
 from users.models import User
 import json
 
@@ -15,55 +15,37 @@ def categoryCreate(request):
         )
         group.save()
         string = '{"Name":"'+categoryData['name']+'"}'
-    return JsonResponse(json.loads(string))
+        return JsonResponse(json.loads(string))
+    return HttpResponseBadRequest("BAD REQUEST")
 
 @csrf_exempt
 def createTask(request):
-    return TaskController.createTask
+    if request.method == 'POST':
+        return TaskController.createTask(request)
+    return HttpResponseBadRequest("BAD REQUEST")
+
+@csrf_exempt
+def deleteTask(request, id):
+    if request.method == 'DELETE':
+        task = Task.objects.get(id=id)
+        task.delete()
+        return HttpResponse("Deleted")
+    return HttpResponseBadRequest("BAD REQUEST")
 
 @csrf_exempt
 def getTask(request, id):
     if request.method == 'GET':
-        token = request.META['HTTP_AUTHORIZATION']
-        task = Task.objects.get(id=id)
-        return HttpResponse(json.dumps(task.serializeCustom(token)), content_type="text/json-comment-filtered")
-    return HttpResponse("OK")
+        return TaskController.getTask(request, id)
+    return HttpResponseBadRequest("BAD REQUEST")
 
 @csrf_exempt
 def getAllTasks(request):
     if request.method == 'GET':
-        token = request.META['HTTP_AUTHORIZATION']
-        tasks = Task.objects.filter(users__id=User.objects.get(token=token).id)
-        response = "{"
-        for task in tasks:
-            response += json.dumps(task.serializeCustom(token)) + ","
-        response += "}"
-        print(response)
-        return HttpResponse(response, content_type="text/json-comment-filtered")
+        return TaskController.getAllTasks(request)
+    return HttpResponseBadRequest("BAD REQUEST")
 
 @csrf_exempt
 def rateTask(request):
     if request.method == 'POST':
-        token = request.META['HTTP_AUTHORIZATION']
-        bodyData = json.loads(request.body)
-        idTask = bodyData['id_tarea']
-        newRating = False
-        try:
-            rating = Rating.objects.get(task__id=idTask, user__token=token)
-        except:
-            newRating = True
-
-        if not newRating:
-            text = bodyData['text'] if bodyData['text'] else rating.text 
-            difficulty = bodyData['dificultad'] if bodyData['dificultad'] else rating.difficulty 
-            utility = bodyData['utilidad'] if bodyData['utilidad'] else rating.utility 
-            rating.text = text
-            rating.difficulty = difficulty
-            rating.utility = utility
-            rating.save()
-        else:
-            rating = Rating(task=Task.objects.get(id=idTask),user=User.objects.get(token=token),text=bodyData['text'],utility=bodyData['utilidad'],difficulty=bodyData['dificultad'])
-            rating.save()
-
-        return JsonResponse(json.loads('{"status":"success", "message":"mensaje valorado correctamente"}'))
-    return HttpResponse("BAD")
+        return TaskController.rateTask(request)
+    return HttpResponseBadRequest("BAD REQUEST")
