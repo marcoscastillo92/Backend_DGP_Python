75fe0828da1343a31cb6217a7b84baa4053b1024
migueg <miguegarciatenorio@gmail.com>
2020-11-14 12:28:20 +0100

    grupos modificados

--- asociacion_vale/groups/models.py
+++ asociacion_vale/groups/models.py
@@ -3,22 +3,12 @@ from datetime import datetime
 from users.models import User
 from ckeditor.fields import RichTextField
 # Create your models here.
-class Category(models.Model):
-    name= models.CharField(max_length=200, verbose_name="Nombre")
-    createdAt = models.DateTimeField(default=datetime.now, verbose_name="Creado en", blank=True)
 
-    class Meta:
-        verbose_name = "Categoría"
-        verbose_name_plural = "Categorías"
-
-    def __str__(self):
-        return self.name
 
 
 class Groups(models.Model):
     name= models.CharField(max_length=150, verbose_name="Nombre")
     memberCount= models.IntegerField(default=0, verbose_name="Contador de miembros")
-    category = models.ForeignKey(Category, verbose_name=("Categoría"), on_delete=models.CASCADE, null=True)
     users = models.ManyToManyField(User, verbose_name="Miembros", blank=True)
     createdAt = models.DateTimeField(default=datetime.now, verbose_name="Creado en", blank=True)
 
@@ -27,12 +17,10 @@ class Groups(models.Model):
         verbose_name_plural = 'Grupos'
 
     def __str__(self):
-        print(self.category)
-        return f"{self.name} - {self.category}"
+        return f"{self.name}"
 
-    def save(self, *args, **kwargs):
-        if self.category is None:  # Set default reference
-            self.category = Category.objects.get(id=1)
+"""  
+         def save(self, *args, **kwargs):
             
         usersRegistredAlready = Groups.objects.getUsers()
         notificacion = True
@@ -44,7 +32,7 @@ class Groups(models.Model):
                 print("Usuario agregado ")
                 #pritnuser.Notify("Grupo")
         super(Groups, self).save(*args, **kwargs)
-
+ """
 class ForumGroup (models.Model):
     name = models.CharField(max_length=150, verbose_name="Nombre")
     Group =  models.ForeignKey(Groups, on_delete=models.CASCADE, null=True, verbose_name="Grupo")
@@ -61,12 +49,10 @@ class MessageForumGroup(models.Model):
     body = RichTextField(verbose_name="Mensaje")
     author = models.ForeignKey(User, on_delete=models.CASCADE, verbose_name="Autor")
     mimeType = models.FileField(upload_to ='uploads/attach', default="null", verbose_name="Tipo de mensaje") 
-    forum = models.ForeignKey(ForumGroup, on_delete=models.CASCADE, null=True, verbose_name="Foro")
+    group = models.ForeignKey(Groups, on_delete=models.CASCADE, null=True, verbose_name="Grupo")
     createdAt = models.DateTimeField(default=datetime.now, blank=True, verbose_name="Fecha de creacion")
 
     def save(self, *args, **kwargs):
-        if self.forum is None:  # Set default reference
-            self.forum = Groups.objects.get(id=1)
         super(MessageForumGroup, self).save(*args, **kwargs)
 
     class Meta:
--- asociacion_vale/groups/urls.py
+++ asociacion_vale/groups/urls.py
@@ -5,6 +5,5 @@ from . import views
 urlpatterns = [
     path('', views.index, name='index'),
     path('create/', views.groupsCreate, name='groupsCreate'),
-    path('create/category', views.categoryCreate, name='categoryCreate'),
     path('get/<int:id>' , views.groupsGet, name='groupsGet')
 ]
